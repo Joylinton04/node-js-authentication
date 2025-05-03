f2a6531e91188be7e63787922f9f83aabfad3700
Joylinton04 <adujoy05@gmail.com>
2025-05-03 22:47:26 +0000

    page setup

---
+++ client/src/route/Approute.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import ResetPassword from '../pages/ResetPassword';
+import VerifyEmail from '../pages/VerifyEmail';
+
+const Approute = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "/reset-password",
+        element: <ResetPassword/>
+    },
+    {
+        path: "/verify-email",
+        element: <VerifyEmail/>
+    },
+])
+
+export default Approute;
