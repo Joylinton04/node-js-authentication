1c41e2ad2a3dfc4ca1b2d8ef405ce3a06afb52ec
Joylinton04 <adujoy05@gmail.com>
2025-05-27 17:01:59 +0000

    code refactor

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -12,6 +12,7 @@ interface AppContextType {
     isAccountVerified: boolean;
   } | null;
   setUserData?: React.Dispatch<React.SetStateAction<null>>;
+  getUserData?: () => Promise<void>;
   getAuthState?: () => Promise<void>;
   canAccessResetPassword: boolean;
   setCanAccessResetPassword: React.Dispatch<React.SetStateAction<boolean>>;
@@ -74,14 +75,14 @@ const AppContextProvider: React.FC<AppContextProviderProps> = ({
     }
   };
 
-  // const getUserData = async () => {
-  //   try {
-  //     const { data } = await axios.get(backendUrl + "/api/user/data");
-  //     data.success ? setUserData(data.userData) : toast.error(data.message);
-  //   } catch (err: any) {
-  //     console.log(err)
-  //   }
-  // };
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/data");
+      data.success ? setUserData(data.userData) : toast.error(data.message);
+    } catch (err: any) {
+      console.log(err)
+    }
+  };
 
 
   // useEffect(() => {
@@ -93,6 +94,7 @@ const AppContextProvider: React.FC<AppContextProviderProps> = ({
     isLoggedIn,
     setIsLoggedIn,
     userData,
+    getUserData,
     setUserData,
     getAuthState,
     canAccessResetPassword,
